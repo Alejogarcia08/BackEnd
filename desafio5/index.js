de805710cce6f3781248f76d3c307036ad93b9da
Alejo Garcia <alejogarciacar@gmail.com>
2022-12-19 21:50:43 -0300

    desafio5

---
+++ desafio5/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const app = express()
+
+const productos = []
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+const PORT = 8070
+
+
+app.get('/', (req, res) => {
+    res.render('inicio', { productos })
+})
+
+app.post('/productos', (req, res) => {
+    productos.push(req.body)
+
+    res.redirect('/')
+})
+
+app.listen(PORT, () => {
+    console.log('server OK!!');
+})
